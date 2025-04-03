096c0c2d46a69a412e0bfdfb9f8ba850e01e0faa
daniluna06 <lunadani06@outlook.com>
2025-04-03 11:12:19 -0400

    Create acct updated to pull from database, and database connection class updated

--- FinancialBloom/DatabaseConnection.java
+++ FinancialBloom/DatabaseConnection.java
@@ -1,4 +1,31 @@
 package csc450;
+import java.sql.*;
+// connection and statement have to be explicitly closed
 
-public class DatabaseConnection {
+public class JDBC {
+    public static void main(String[] args) throws SQLException {
+        String userName = "mmr4541@uncw.edu";
+        String password = "Softwareengineering450";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // Use this method to get a connection from anywhere in your app
+        public static Connection getConnection() throws SQLException {
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        }
+        try {
+            // check the actual ip addr, what is the port num, and what is the db name?
+            Connection connection = DriverManager.getConnection("jdbc:mysql://192.168.01:3306/database_name",
+                    userName, password);
+            System.out.println("Connected to the database!");
+            connection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
