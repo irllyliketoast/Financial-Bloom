064e4439735d21d69c241da04485c65552c27333
Temo Galindo <153858347+xvexx117@users.noreply.github.com>
2025-04-27 22:06:42 -0400

    Update milestone_driver.java
    Signed-off-by: Temo Galindo <153858347+xvexx117@users.noreply.github.com>

--- FinancialBloom/src/milestone_driver.java
+++ FinancialBloom/src/milestone_driver.java
@@ -4,6 +4,7 @@ This code is the driver file for the Milestone class. Shows off many of the clas
 correct implementation of setters and getters, and the right rollout of mehtods like balance and calculateRemaining.
 
 Update Log
+Updated on 4.27.25 by Temo Galindo - Added 7th test for Complete instance variable and functionality
 Created on 4.14.2025 by Temo Galindo
 */
 package csc450;
@@ -26,33 +27,42 @@ public class milestone_driver {
         }
         Milestone[] milestones = {milestone1, milestone2};
         System.out.println("Test 1 - Instantiating with the overloaded constructors.");
-        System.out.printf("%s:\n Name: %s\n Total Amount: $%.2f\n Amount Paid: $%.2f\n Monthly Contribution: $%.2f\n","Milestone 1 - no amount paid/monthly contribution", milestone1.getName(), milestone1.getAmount(), milestone1.getAmountPaid(), milestone1.getContribution());
-        System.out.printf("%s:\n Name: %s\n Total Amount: $%.2f\n Amount Paid: $%.2f\n Monthly Contribution: $%.2f\n","Milestone 2 - Amount paid and monthly contribution set", milestone2.getName(), milestone2.getAmount(), milestone2.getAmountPaid(), milestone2.getContribution());
+        System.out.printf("%s:\nName: %s\nTotal Amount: $%.2f\nAmount Paid: $%.2f\nMonthly Contribution: $%.2f\n","Milestone 1 - no amount paid/monthly contribution", milestone1.getName(), milestone1.getAmount(), milestone1.getAmountPaid(), milestone1.getContribution());
+        System.out.printf("%s:\nName: %s\nTotal Amount: $%.2f\nAmount Paid: $%.2f\nMonthly Contribution: $%.2f\n","Milestone 2 - Amount paid and monthly contribution set", milestone2.getName(), milestone2.getAmount(), milestone2.getAmountPaid(), milestone2.getContribution());
         System.out.println("Test 2 - Balance Method: Total amount - Amount Paid");
         for(Milestone mile: milestones){
             System.out.printf("%s: %.2f\n",mile.getName(),mile.getBalance());
         }
         System.out.println("Test 3 - Setting new amount paid and contribution.");
-        System.out.printf("%s:\n Name: %s\n Total Amount: $%.2f\n Amount Paid: $%.2f\n Monthly Contribution: $%.2f\n","No amount paid/monthly contribution", milestone1.getName(), milestone1.getAmount(), milestone1.getAmountPaid(), milestone1.getContribution());
+        System.out.printf("%s:\n Name: %s\n Total Amount: $%.2f\nAmount Paid: $%.2f\n Monthly Contribution: $%.2f\n","No amount paid/monthly contribution", milestone1.getName(), milestone1.getAmount(), milestone1.getAmountPaid(), milestone1.getContribution());
         milestone1.setAmountPaid(1248.97);
         milestone1.setContribution(378.56);
-        System.out.printf("%s:\n Name: %s\n Total Amount: $%.2f\n Amount Paid: $%.2f\n Monthly Contribution: $%.2f\n","Newly set amount paid/monthly contribution", milestone1.getName(), milestone1.getAmount(), milestone1.getAmountPaid(), milestone1.getContribution());
+        System.out.printf("%s:\n Name: %s\nTotal Amount: $%.2f\n Amount Paid: $%.2f\n Monthly Contribution: $%.2f\n","Newly set amount paid/monthly contribution", milestone1.getName(), milestone1.getAmount(), milestone1.getAmountPaid(), milestone1.getContribution());
         System.out.println("Test 4 - Adding amount paid using monthly contribution.");
-        System.out.printf("Name: %s\n Total Amount: $%.2f\n Monthly Contribution: $%.2f\n Amount Paid: $%.2f\n Balance: %.2f\n", milestone2.getName(), milestone2.getAmount(), milestone2.getContribution(), milestone2.getAmountPaid(),milestone2.getBalance());
+        System.out.printf("Name: %s\nTotal Amount: $%.2f\nMonthly Contribution: $%.2f\n Amount Paid: $%.2f\n Balance: %.2f\n", milestone2.getName(), milestone2.getAmount(), milestone2.getContribution(), milestone2.getAmountPaid(),milestone2.getBalance());
         milestone2.addContribution();
-        System.out.printf("Name: %s\n Total Amount: $%.2f\n Monthly Contribution: $%.2f\n Amount Paid: $%.2f\n Balance: %.2f\n", milestone2.getName(), milestone2.getAmount(), milestone2.getContribution(), milestone2.getAmountPaid(),milestone2.getBalance());
+        System.out.printf("Name: %s\nTotal Amount: $%.2f\nMonthly Contribution: $%.2f\n Amount Paid: $%.2f\n Balance: %.2f\n", milestone2.getName(), milestone2.getAmount(), milestone2.getContribution(), milestone2.getAmountPaid(),milestone2.getBalance());
         System.out.println("Test 5 - Adding amount paid using any amount contribution.");
-        System.out.printf("Name: %s\n Total Amount: $%.2f\n Amount Paid: $%.2f\n Balance: $%.2f\n", milestone1.getName(), milestone1.getAmount(), milestone1.getAmountPaid(), milestone1.getBalance());
+        System.out.printf("Name: %s\nTotal Amount: $%.2f\nAmount Paid: $%.2f\nBalance: $%.2f\n", milestone1.getName(), milestone1.getAmount(), milestone1.getAmountPaid(), milestone1.getBalance());
         System.out.println("---Adding $2000 to amount paid.---");
-        milestone1.addAmounPaid(2000.00);
-        System.out.printf("Name: %s\n Total Amount: $%.2f\n Amount Paid: $%.2f\n Balance: $%.2f\n", milestone1.getName(), milestone1.getAmount(), milestone1.getAmountPaid(), milestone1.getBalance());
+        milestone1.addAmountPaid(2000.00);
+        System.out.printf("Name: %s\nTotal Amount: $%.2f\nAmount Paid: $%.2f\nBalance: $%.2f\n", milestone1.getName(), milestone1.getAmount(), milestone1.getAmountPaid(), milestone1.getBalance());
         System.out.println("Test 6 - Using Calculate Remaining Months method.");
         for(Milestone mile: milestones){
-            System.out.printf("Name: %s\n Total Amount: %.2f\n Amount Paid: %.2f\n Monthly Contribution: %.2f\n Months Until Milestone is Reached: %d\n", mile.getName(), mile.getAmount(), mile.getAmountPaid(), mile.getContribution(), mile.calculateRemaining());
+            System.out.printf("Name: %s\nTotal Amount: %.2f\nAmount Paid: %.2f\nMonthly Contribution: %.2f\nMonths Until Milestone is Reached: %d\n", mile.getName(), mile.getAmount(), mile.getAmountPaid(), mile.getContribution(), mile.calculateRemaining());
         }
+        System.out.println("Test 7 - Testing Complete Status ");
+        System.out.printf("Name: %s\nTotal Amount: $%.2f\nAmount Paid: $%.2f\nStatus: %b\n", milestone1.getName(), milestone1.getAmount(), milestone1.getAmountPaid(), milestone1.isComplete());
+        milestone1.addContribution();
+        milestone1.addContribution();
+        milestone1.addContribution();
+        milestone1.addContribution();
+        System.out.printf("Name: %s\nTotal Amount: $%.2f\nAmount Paid: $%.2f\nStatus: %b\n", milestone1.getName(), milestone1.getAmount(), milestone1.getAmountPaid(), milestone1.isComplete());
+        milestone1.setAmountPaid(0.0);
+        System.out.printf("Name: %s\nTotal Amount: $%.2f\nAmount Paid: $%.2f\nStatus: %b\n", milestone1.getName(), milestone1.getAmount(), milestone1.getAmountPaid(), milestone1.isComplete());
+        milestone1.setAmountPaid(4500.00);
+        System.out.printf("Name: %s\nTotal Amount: $%.2f\nAmount Paid: $%.2f\nStatus: %b\n", milestone1.getName(), milestone1.getAmount(), milestone1.getAmountPaid(), milestone1.isComplete());
         System.out.println("--Testing Complete--");
-        
-
         
         
     }
