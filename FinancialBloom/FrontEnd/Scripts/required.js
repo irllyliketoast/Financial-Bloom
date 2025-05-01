9c171dc52a62645c9555aa5b95ef0dfaa54382f2
xvexx117 <temoji123@gmail.com>
2025-05-01 01:31:04 -0400

    added settings api call + refactored functions

---
+++ FinancialBloom/FrontEnd/Scripts/required.js
@@ -0,0 +1,17 @@
+function getCookie(name) { // returns userid when you do 'getCookie("userId")'
+    const cookieString = document.cookie;
+    const cookies = cookieString.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.startsWith(name + '=')) {
+        return cookie.substring(name.length + 1);
+      }
+    }
+    return null;
+  }
+// 🖱️ Custom Cursor Follower Script
+const cursor = document.getElementById('customCursor');
+document.addEventListener('mousemove', (e) => {
+  cursor.style.left = `${e.clientX}px`;
+  cursor.style.top = `${e.clientY}px`;
+});
