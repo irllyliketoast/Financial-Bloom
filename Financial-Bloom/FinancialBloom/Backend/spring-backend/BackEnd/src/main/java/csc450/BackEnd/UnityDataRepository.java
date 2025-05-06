948cb762543d2f1ee674cd4bb6873c2c55d8a597
Laura Estremera <149011375+irllyliketoast@users.noreply.github.com>
2025-05-06 09:12:58 -0400

    Delete Financial-Bloom directory
    Signed-off-by: Laura Estremera <149011375+irllyliketoast@users.noreply.github.com>

--- Financial-Bloom/FinancialBloom/Backend/spring-backend/BackEnd/src/main/java/csc450/BackEnd/UnityDataRepository.java
+++
@@ -1,9 +0,0 @@
-package csc450.BackEnd;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UnityDataRepository extends JpaRepository<UnityDataRequest, Long> {
-    // Optional: Add custom queries here
-}
--- Financial-Bloom/FinancialBloom/src/DatabaseConnection.java
+++
@@ -1,9 +0,0 @@
-/*  
-Code Author: Daniela Luna  
-Description Author: Laura Estremera  
-*/
-
-package csc450;
-
-public class DatabaseConnection {
-}
